c66207df9f5e12f6adb1bfdbc5b3db48b69675d4
B269905-2024 <s2713107@ed.ac.uk>
2025-05-29 17:08:06 +0000

    added script for downloading AF structures using uniprotid

--- download_pdb.sh
+++ download_pdb.sh
@@ -3,16 +3,16 @@
 mkdir -p alphafold_structures
 
 while read -r uniprot_id; do
-  echo " Downloading AlphaFold structure for $uniprot_id..."
+  echo "downloading af structure for $uniprot_id"
 
   url="https://alphafold.ebi.ac.uk/files/AF-${uniprot_id}-F1-model_v4.pdb"
   output="alphafold_structures/${uniprot_id}.pdb"
 
   if wget -q --spider "$url"; then
     wget -q "$url" -O "$output"
-    echo " Saved to $output"
+    echo " saved as $output"
   else
-    echo " Structure not found for $uniprot_id"
+    echo "$uniprot_id download failed"
   fi
 
 done < uniprot_ids.txt
